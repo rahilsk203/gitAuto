88e905afe67d56539d812b9b106ff0dae4fee9bd
root <root@users.noreply.github.com>
2025-11-06 07:22:26 -0800

    Auto commit - 2025-11-06

--- test-deps.js
+++
@@ -1,9 +0,0 @@
-const dependencyChecker = require('./lib/dependency-checker');
-
-async function test() {
-  console.log('Testing dependency checker...');
-  const result = await dependencyChecker.checkAndInstallDependencies();
-  console.log('Dependency check result:', result);
-}
-
-test();
